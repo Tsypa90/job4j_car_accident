83f01ae6b602143d212cbdd9a435f332d67e9856
Tsypa90 <URSA2004@mail.ru>
2022-08-23 10:14:50 +0300

    0. Spring DataSource [#6878 #335096]

---
+++ db/002_dml_insert_data.sql
@@ -0,0 +1,7 @@
+insert into types (name) values ('Две машины');
+INSERT into types (name) VALUES ('Машина и человек');
+INSERT into types (name) values ('Машина и велосипед');
+
+insert into rules (name) values ('Статья. 1');
+insert into rules (name) values ('Статья. 2');
+insert into rules (name) values ('Статья. 3');
